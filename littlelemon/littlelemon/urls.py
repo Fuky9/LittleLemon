0fd9289d3b15c3ddc0c81834632169644b8fa032
Fuky9 <lukasfukan6@gmail.com>
2024-02-28 21:05:22 +0100

    Added DRF settings and Djoser, set authentication and secured BookingViewSet

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
